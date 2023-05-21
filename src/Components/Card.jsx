23f14a950245ca6992437740722130dc97201bf7
Oscar Meneses <omenesess1800@alumno.ipn.mx>
2023-05-20 21:08:35 -0600

    add an add to cart button in the product detail

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,58 +2,59 @@ import { useContext } from 'react'
 import { ShoopingCartContext } from '../Context/Context'
 import { PlusIcon, CheckIcon } from '@heroicons/react/24/solid'
 
+
+
+
 const Card = (data) => {
 
-    const context = useContext(ShoopingCartContext)
+  const context = useContext(ShoopingCartContext)
 
-    const showProductDetail = (productDetail) => {
-        context.openProductDetail()
-        context.closeCheckoutSideMenu()
-        context.setProductShow(productDetail)
-    }
+  const showProductDetail = (productDetail) => {
+    context.openProductDetail()
+    context.closeCheckoutSideMenu()
+    context.setProductShow(productDetail)
+  }
 
-    const addProductToCart = (productData) => {
-        context.setCount(context.count + 1)
-        context.setCartProducts([...context.cartProducts, productData])
-        context.openCheckoutSideMenu()
-        context.closeProductDetail()
-    }
+  const addProductToCart = (productData) => {
+    context.setCount(context.count + 1)
+    context.setCartProducts([...context.cartProducts, productData])
+  }
 
-    const renderIcon = (id) => {
-        const isInCart = context.cartProducts.filter(product => product.id === id).length > 0
-    
-        if (isInCart) {
-          return (
-            <div
-              className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'>
-              <CheckIcon className='h-6 w-6 text-green-500'></CheckIcon>
-            </div>
-          )
-        } else {
-          return (
-            <div
-              className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'
-              onClick={() => addProductToCart(data.data)}>
-              <PlusIcon className='h-6 w-6 text-black'></PlusIcon>
-            </div>
-          )
-        }
-      }
+  const renderIcon = (id) => {
+    const isInCart = context.cartProducts.filter(product => product.id === id).length > 0
 
-    return (
-        <div className='relative bg-white cursor-pointer w-46 h-50'>
-            <figure className='relative mb-2 w-full h-4/5 '
-                onClick={() => showProductDetail(data.data)}>
-                <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>{data.data.category.name}</span>
-                <img className='w-full h-full object-cover rounded-lg' src={data.data.images[0]} alt={data.data.title} />
-                <p className='flex justify-between'>
-                    <span className='text-lg font-light'>{data.data.title}</span>
-                    <span className='text-lg font-medium'>${data.data.price}</span>
-                </p>
-            </figure>
-            {renderIcon(data.data.id)}
+    if (isInCart) {
+      return (
+        <div
+          className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'>
+          <CheckIcon className='h-6 w-6 text-green-500'></CheckIcon>
         </div>
-    )
+      )
+    } else {
+      return (
+        <div
+          className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'
+          onClick={() => addProductToCart(data.data)}>
+          <PlusIcon className='h-6 w-6 text-black'></PlusIcon>
+        </div>
+      )
+    }
+  }
+
+  return (
+    <div className='relative bg-white cursor-pointer w-46 h-50'>
+      <figure className='relative mb-2 w-full h-4/5 '
+        onClick={() => showProductDetail(data.data)}>
+        <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>{data.data.category.name}</span>
+        <img className='w-full h-full object-cover rounded-lg' src={data.data.images[0]} alt={data.data.title} />
+        <p className='flex justify-between'>
+          <span className='text-lg font-light'>{data.data.title}</span>
+          <span className='text-lg font-medium'>${data.data.price}</span>
+        </p>
+      </figure>
+      {renderIcon(data.data.id)}
+    </div>
+  )
 }
 
 export default Card
--- src/Components/CheckoutSideMenu.jsx
+++ src/Components/CheckoutSideMenu.jsx
@@ -14,15 +14,17 @@ const CheckoutSideMenu = () => {
         context.setCount(context.count - 1)
     }
 
-    return (
-        <aside className={`${context.isCheckoutSideMenuView ? 'flex' : 'hidden'} checkout-side-menu scrollable-cards flex-col fixed right-0 border border-black rounded-lg bg-white`}>
-            <div className='flex justify-between items-center p-3'>
-                <h2 className='text-xl font-medium'>My Order</h2>
-                <div className='cursor-pointer' onClick={() => context.closeCheckoutSideMenu()}>
-                    <XMarkIcon className="w-6 h-6 text-black cursor-pointer" />
+    const renderCarts = () => {
+        if (context.cartProducts.length === 0) {
+            return (
+                <div className='flex flex-col  justify-center items-center h-full gap-1'>
+                    <p className='text-2xl font-light italic text-black/60'>Vacio</p>
+                    <p className='text-2xl font-light italic text-black/60'>Agrega algo al carrito </p>
                 </div>
-            </div>
-            {
+            )
+        }
+        else {
+            return (
                 context.cartProducts.map((product) => (<OrderCard
                     key={product.id}
                     id={product.id}
@@ -32,7 +34,19 @@ const CheckoutSideMenu = () => {
                     handleDelete={handleDelete}
                 />
                 ))
-            }
+            )
+        }
+    }
+
+    return (
+        <aside className={`${context.isCheckoutSideMenuView ? 'flex' : 'hidden'} checkout-side-menu scrollable-cards flex-col fixed right-0 border border-black rounded-lg bg-white`}>
+            <div className='flex justify-between items-center p-3'>
+                <h2 className='text-xl font-medium'>My Order</h2>
+                <div className='cursor-pointer' onClick={() => context.closeCheckoutSideMenu()}>
+                    <XMarkIcon className="w-6 h-6 text-black cursor-pointer" />
+                </div>
+            </div>
+            {renderCarts()}
         </aside>
     )
 }
